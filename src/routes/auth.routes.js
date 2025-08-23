41f7e649fddbf7f9c3d7423a032d7414ff758d3e
BerrioA <Ingalejandroberrio@gmail.com>
2025-08-22 19:20:09 -0500

    correcion de nombre de controlador de autenticarion y refreshtoken

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { login, logout } from "../controllers/auth.controller.js";
-import { refreshToken } from "../controllers/refreshToken.Controller.js";
+import { refreshToken } from "../controllers/refreshToken.controller.js";
 import { requireRefreshToken } from "../middlewares/auth/requireRefreshToken.js";
 import { profile } from "../controllers/user.controller.js";
 import { requireToken } from "../middlewares/auth/requireToken.js";
