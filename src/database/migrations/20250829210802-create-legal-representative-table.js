07fd563e08b404a11570b7f703384d05ad869c73
BerrioA <ingalejandroberrio@gmail.com>
2025-11-19 07:21:14 -0500

    migración de representante legal

---
+++ src/database/migrations/20250829210802-create-legal-representative-table.js
@@ -0,0 +1,40 @@
+import { DataTypes } from "sequelize";
+
+export async function up({ context }) {
+  await context.createTable("legal_representative", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.UUID,
+      allowNull: true,
+      references: {
+        model: "users",
+        key: "id",
+      },
+      onUpdate: "CASCADE",
+      onDelete: "SET NULL",
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    updated_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    deleted_at: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+  });
+}
+
+export async function down({ context }) {
+  await context.dropTable("legal_representative");
+}
