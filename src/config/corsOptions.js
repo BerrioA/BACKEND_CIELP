c0b11d1a4038af2fa41386d52079c83aa76d4f15
BerrioA <ingalejandroberrio@gmail.com>
2025-12-05 15:04:24 -0500

    Permiso de frontend en produccion

--- src/config/corsOptions.js
+++ src/config/corsOptions.js
@@ -1,6 +1,6 @@
 // ⚡ Configurar CORS
 export const corsOptions = {
-  origin: ["http://localhost:5173"],
+  origin: ["http://localhost:5173", "https://cielp.vercel.app"],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
