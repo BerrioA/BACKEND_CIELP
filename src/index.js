cffcb1effcf2eb037a3b9e3fcab668c366f8a71b
BerrioA <ingalejandroberrio@gmail.com>
2025-12-05 14:53:50 -0500

    Integracion de migraciones a produccion

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ async function main() {
 
     // await sequelize.sync({ alter: true });
 
-    // await migrator.up();
+    await migrator.up();
     // await seeder.up();
 
     if (NODE_ENV !== "test") {
