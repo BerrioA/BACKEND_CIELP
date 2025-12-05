138ff39f224fc25505bbe612136875bd06200c96
BerrioA <ingalejandroberrio@gmail.com>
2025-12-05 15:19:44 -0500

    correccion de seeders para los espacios a integar

--- src/database/seeders/20250831100003-initial-spaces.js
+++ src/database/seeders/20250831100003-initial-spaces.js
@@ -10,7 +10,7 @@ export async function up({ context }) {
         description:
           "La Sala de Entrenamiento en Comunicación Terapéutica es un espacio dirigido a personas con miedo a hablar en público o ansiedad social. Mediante técnicas psicológicas y ejercicios guiados, ayuda a mejorar la confianza, la expresión y la seguridad al comunicarse.",
         url: "https://www.spatial.io/s/Sala-Cielp-692c7b46ab39474d1b19bcc4?share=281102136189982839",
-        imageUrl:
+        image_url:
           "https://s3.ppllstatics.com/elcorreo/www/multimedia/202111/25/media/cortadas/metaverso-kgJH-U16032571521FmF-1248x770@RC.jpg",
         created_at: new Date(),
         updated_at: new Date(),
