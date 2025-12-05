29b968596d50f98c5f23c723c64e2b3d57dc17b4
BerrioA <ingalejandroberrio@gmail.com>
2025-12-05 15:19:01 -0500

    correccion de controlador para mostrar los espacios en Migracion de espacios

--- src/services/space.services.js
+++ src/services/space.services.js
@@ -3,7 +3,7 @@ import { Space } from "../models/spaces.model.js";
 export const getAllSpaces = async () => {
   try {
     const spaces = await Space.findAll({
-      attributes: ["id", "title", "description", "url", "imageUrl"],
+      attributes: ["id", "title", "description", "url", "image_url"],
       order: [["title", "ASC"]],
     });
 
