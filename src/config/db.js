c26ac46ec947d87c51f2e83202f0dbfeb6f20a3b
BerrioA <ingalejandroberrio@gmail.com>
2025-12-05 14:52:01 -0500

    Correccion del ssl para Render

--- src/config/db.js
+++ src/config/db.js
@@ -3,5 +3,12 @@ import { POSTGRES_URL } from "./env.js";
 
 export const sequelize = new Sequelize(POSTGRES_URL, {
   dialect: "postgres",
+  protocol: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // Importante para Render
+    },
+  },
   logging: false,
 });
